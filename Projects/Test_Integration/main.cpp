5530127304c84c2089c53b580b5f92bf4b66a9f2
Avi Robinson-Mosher <avi.mosher@gmail.com>
2014-12-05 18:08:43 -0500

    adding program that actually calls simulation stuff.  Now we're getting somewhere.

---
+++ Projects/Test_Integration/main.cpp
@@ -0,0 +1,20 @@
+#include <Data/DATA.h>
+#include <Driver/DRIVER.h>
+#include <Evolution/EVOLUTION.h>
+#include <Force/FORCE.h>
+#include <cerrno>
+#include <fstream>
+#include <iostream>
+#include <string>
+
+using namespace Mechanics;
+int main()
+{
+    typedef double T;
+    typedef Matrix<T,1,1> TV;
+    DATA<TV> data;
+    EVOLUTION<TV> evolution;
+    FORCE<TV> force;
+    DRIVER<TV> driver(data,evolution,force);
+    return 0;
+}
