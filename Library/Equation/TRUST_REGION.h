d12e5877b6aeb6d1e89117f17e51d0e10a1b3786
Avi Robinson-Mosher <avi.mosher@gmail.com>
2015-03-11 02:03:16 -0400

    trust region header

---
+++ Library/Equation/TRUST_REGION.h
@@ -0,0 +1,69 @@
+#ifndef __TRUST_REGION__
+#define __TRUST_REGION__
+
+#include <Evolution/EVOLUTION_STEP.h>
+
+namespace Mechanics{
+    template<class TV> class EQUATION;
+    
+template<class TV>
+class TRUST_REGION:public EVOLUTION_STEP<TV>
+{
+    typedef typename TV::Scalar T;
+    typedef Matrix<T,Dynamic,1> Vector;
+    typedef SimplicialLLT<SparseMatrix<T>> Preconditioner;
+public:
+    enum STATUS{UNKNOWN,CONTINUE,SUCCESS,EMAXITER,ETOLG,MOVED,EXPAND,CONTRACT,FAILEDCG,ENEGMOVE};
+    EQUATION<TV>* equation;
+    SparseMatrix<T> Bk;
+    Preconditioner PrecondLLt;
+    Vector xk;
+    Vector gk;
+    Vector sk;
+    Vector yk;
+    Vector try_x;
+    Vector try_g;
+    Vector zj;
+    Vector rj;
+    Vector dj;
+    Vector zj_old;
+    Vector yj;
+    Vector wd;
+    Vector wz;
+    T try_f,gs;
+    T f;
+    T norm_gk,ared,pred,sBs,ap,norm_sk_scaled;
+    
+    T radius;
+    T min_radius;
+    T tol;
+    T prec;
+    T contract_factor;
+    T expand_factor;
+    T contract_threshold;
+    T expand_threshold_ap;
+    T expand_threshold_rad;
+    T function_scale_factor;
+    int preconditioner_refresh_frequency;
+    int max_iterations,num_CG_iterations,trust_iterations;
+    int nvars;
+    std::string CG_stop_reason;
+
+    TRUST_REGION();
+    ~TRUST_REGION(){}
+
+    void Step(SIMULATION<TV>& simulation,const T dt,const T time);
+    void Update_Preconditioner();
+    void Update_Hessian();
+    void Update_One_Step();
+    void Solve_Trust_CG();
+    bool Finite(T x);
+    T Get_Norm_Sk(const Preconditioner& X);
+    void Get_F(const Vector& x,T& f);
+    void Gradient(const Vector& x,Vector& g);
+    void UPz(const Preconditioner& X,const Vector& v,Vector& out);
+    T Find_Tau(const Vector& z,const Vector& d);
+    void Get_FDF();
+};
+}
+#endif
