2b25d9bebdeda0565a9928d1a6cd4c70ccab9a64
Avi Robinson-Mosher <avi.mosher@gmail.com>
2015-06-18 17:06:14 -0400

    fix install script

--- install.sh
+++ install.sh
@@ -1,26 +1,50 @@
 #!/bin/bash
 
-mkdir External_Libraries
+if [ ! -d External_Libraries ]
+then
+    mkdir External_Libraries
+fi
 cd External_Libraries
-wget https://github.com/USCiLab/cereal/archive/v1.1.1.tar.gz
-tar -xvf v1.1.1.tar.gz
-mv cereal-1.1.1 cereal
-wget http://bitbucket.org/eigen/eigen/get/3.2.4.tar.gz
-tar -xvf 3.2.4.tar.gz
-mv eigen-eigen-10219c95fe65 eigen
-git clone git@github.com:open-source-parsers/jsoncpp.git
+
+if [ ! -d cereal ]
+then
+    wget https://github.com/USCiLab/cereal/archive/v1.1.1.tar.gz
+    tar -xvf v1.1.2.tar.gz
+    mv cereal-1.1.2 cereal
+fi
+
+if [ ! -d eigen ]
+then
+    wget http://bitbucket.org/eigen/eigen/get/3.2.4.tar.gz
+    tar -xvf 3.2.4.tar.gz
+    mv eigen-eigen-10219c95fe65 eigen
+fi
+
+if [ ! -d jsoncpp ]
+then
+    git clone git@github.com:open-source-parsers/jsoncpp.git
+fi
+
 cd jsoncpp
 git checkout tags/1.6.2
 cmake .
 make
 python amalgamate.py
-cp dist/libjsoncpp.so ../../bin/
+g++ -o dist/jsoncpp.os -c -fPIC -Idist dist/jsoncpp.cpp
+g++ -o ../../bin/libjsoncpp.so -shared dist/jsoncpp.os
 cd ..
-wget http://www.openscenegraph.org/downloads/stable_releases/OpenSceneGraph-3.0/source/OpenSceneGraph-3.0.0.zip
-unzip OpenSceneGraph-3.0.0.zip
-mv OpenSceneGraph-3.0.0 osg
+
+if [ ! -d osg ]
+then
+    wget http://www.openscenegraph.org/downloads/stable_releases/OpenSceneGraph-3.0/source/OpenSceneGraph-3.0.0.zip
+    unzip OpenSceneGraph-3.0.0.zip
+    mv OpenSceneGraph-3.0.0 osg
+fi
+
 cd osg
 ./configure
 make
-cd ../..
+cd ..
+
+cd ..
 scons
