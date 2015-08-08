745c7215b2921d90d4087aaf259291baf697971f
Avi Robinson-Mosher <avi.mosher@gmail.com>
2015-08-08 03:00:08 -0400

    fix up preconditioner by fixing Find Tau - yet another bug from that original code

--- Library/Equation/TRUST_REGION.cpp
+++ Library/Equation/TRUST_REGION.cpp
@@ -153,7 +153,7 @@ template<class TV> void TRUST_REGION<TV>::
 Update_Preconditioner()
 {
     nvars=hessian.rows();
-#if 1
+#if 0
     Vector TT(nvars);
     SparseMatrix<T> BB(nvars,nvars);
     BB.setIdentity();
@@ -178,8 +178,8 @@ Update_One_Step()
     auto step_status=UNKNOWN;
     T try_f,step_quality,predicted_reduction;
     Solve_Trust_CG(sk);
-    LOG::cout<<"sk: "<<sk.transpose()<<std::endl;
     T norm_sk_scaled=Get_Norm_Sk(PrecondLLt);
+    LOG::cout<<"sk: norm: "<<norm_sk_scaled<<std::endl<<sk.transpose()<<std::endl;
     if(!finite(norm_sk_scaled)){step_status=FAILEDCG;}
     else{
         Linearize_Around();
@@ -223,6 +223,7 @@ Update_One_Step()
             T gamma_bad=(1-contract_threshold)*gksk/((1-contract_threshold)*(f+gksk+contract_threshold*(f-predicted_reduction)-try_f));
             LOG::cout<<"gamma bad: "<<gamma_bad<<" gksk: "<<gksk<<std::endl;
             radius=std::min(contract_factor*norm_sk_scaled,std::max((T)0.0625,gamma_bad)*radius);
+            //radius=std::min(contract_factor*norm_sk_scaled,std::max((T)0.0625,gamma_bad)*radius);
             step_status=CONTRACT;
             break;}
         case CONTRACT:
@@ -275,6 +276,7 @@ Solve_Trust_CG(Vector& pk)
 
         UPz(PrecondLLt,zj,wd);
         norm_zj=wd.norm();
+        LOG::cout<<"norm_zk: "<<norm_zj<<std::endl;
 
         if(norm_zj>=radius){
             // find tau>=0 s.t. p intersects trust region
@@ -318,7 +320,8 @@ Solve_Trust_CG(Vector& pk)
 template<class TV> typename TV::Scalar TRUST_REGION<TV>::
 Get_Norm_Sk(const Preconditioner& X)
 {
-    T res=(X.matrixU()*(X.permutationPinv()*sk).eval()).norm();
+    //T res=(X.matrixU()*(X.permutationP()*sk).eval()).norm();
+    T res=(X.matrixU().template triangularView<Upper>()*(X.permutationP()*sk).eval()).norm();
     return res;
 }
 ///////////////////////////////////////////////////////////////////////
@@ -337,8 +340,10 @@ Get_Gradient(const Vector& x,Vector& g)
 template<class TV> void TRUST_REGION<TV>::
 UPz(const Preconditioner& X,const Vector& v,Vector& out)
 {
-    //out=X.permutationP()*v;
-    //out=X.matrixU().template triangularView<Upper>()*out;
+    //T res=(X.permutationPinv()*X.matrixL()*X.matrixU()*X.permutationP()*sk).norm();
+
+    /*out=X.permutationP()*v;
+      out=X.matrixU().template triangularView<Upper>()*out;*/
     // it looks like X.matrixU() is L...
     out=X.permutationP()*v;
     out=X.matrixU()*out;
@@ -364,7 +369,8 @@ Find_Tau(const Vector& z,const Vector& d)
     T pCd=z.dot(wd);
     T dCd=d.dot(wd);
     T pCp=z.dot(wz);
-    T tau=(-2*pCd+sqrt(pCd*pCd-4*dCd*(pCp-radius*radius)))/(2*dCd);
+    T tau=(-2*pCd+sqrt(4*pCd*pCd-4*dCd*(pCp-radius*radius)))/(2*dCd);
+    LOG::cout<<"Tau: "<<tau<<std::endl;
     return tau;
 }
 ///////////////////////////////////////////////////////////////////////
