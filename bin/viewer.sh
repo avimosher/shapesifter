5b83ff3e35627332648b3a1b77656a6de4145573
Avi Robinson-Mosher <avi.mosher@gmail.com>
2016-06-22 15:42:46 -0400

    disable GPU blacklist

--- bin/viewer.sh
+++ bin/viewer.sh
@@ -2,4 +2,4 @@
 
 args=("$*")
 script=$(cat)
-echo "$script" | ./expand_scene.sh | ./viewer-js/node_modules/electron-prebuilt/cli.js ./viewer-js/main.js
+echo "$script" | ./expand_scene.sh | ./viewer-js/node_modules/electron-prebuilt/cli.js --ignore-gpu-blacklist ./viewer-js/main.js
